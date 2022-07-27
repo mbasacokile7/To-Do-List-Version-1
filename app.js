e7e3c3eb45e1395105842ddb793561e35a19cb72
Mbasa Cokile <mbasacokile7@gmail.com>
2022-07-27 05:12:59 +0200

    Add Groceries List plus Grocery List Route

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ let options = { weekday: "long", month:"long", day:"numeric", year:"numeric"};
 let day = today.toLocaleDateString("en-GB", options);
 let items = ["Buy Food", "Cook Food", "Eat Food"];
 let workItems = [];
+let groceryItems = [];
 let newListItem = "";
 
 
@@ -27,22 +28,39 @@ app.get("/", function(req, res){
     res.render("list", {listTitle:day, listItem:items});
 });
 
+//Work List
 app.get("/work", function(req, res){
     res.render("list", {listTitle: "Work", listItem:workItems});
 });
 
-app.post("/", function(req, res){
-    console.log(req.body);
+//Grocery List
+app.get("/groceries", function(req, res){
+    res.render("list", {listTitle: "Groceries", listItem:groceryItems});
+});
 
-    if(req.body.list === "Work"){
-        newListItem = req.body.newListItem;
-        workItems.push(newListItem);
-        res.redirect("/work");
-    } else{
-        newListItem = req.body.newListItem;
-        items.push(newListItem)
-        res.redirect("/");
+app.post("/", function(req, res){
+    // Check which list we are working on so that we can append the data store accordingly
+    switch (req.body.list) {
+        case "Work":
+            newListItem = req.body.newListItem;
+            workItems.push(newListItem);
+            res.redirect("/work");
+            break;
+        
+        case "Groceries":
+            newListItem = req.body.newListItem;
+            groceryItems.push(newListItem);
+            res.redirect("/groceries");
+            break;
+        
+
+        default:
+            newListItem = req.body.newListItem;
+            items.push(newListItem)
+            res.redirect("/");
+            break;
     }
+
     
     //res.send(newListItem);
 });
